0320f653d841b1061958ea2c7d67a3ef02b1571b
ebssgd <iamebssgd@yahoo.com>
2021-09-10 17:00:12 -0500

    Final commit

---
+++ models/Document.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const documentSchema = new mongoose.Schema({
+  value: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Document", documentSchema);
